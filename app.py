783daacdb99d98e56e84da232ac6428bf142c07f
yyuto6012 <(yyabu0618@gmail.com)>
2017-10-20 16:13:49 -0500

    fix the error

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Digital Binding, LLC'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run('127.0.0.1', port = 8080, debug=True)
