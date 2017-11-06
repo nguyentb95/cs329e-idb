77e58dac553ad8607f0308403db77ccec03ad1f7
sdtran <steventran@utexas.edu>
2017-11-05 20:12:31 -0600

    Updated models with relationships

--- Models.py
+++ Models.py
@@ -3,20 +3,28 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 
+#Book table
 class Book(db.Model):
 	ISBN = db.Column(db.Integer, primary_key=True)
 	Title = db.Column(db.String(80), unique=True, nullable=False)
-	Author_id = db.Column(db.Integer, db.ForeignKey('author.Author_id'))
-	Publisher_id = db.Column(db.Integer)
+
+	Author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
+	author = db.relationship('Author', backref=db.backref('books', lazy = True))
+
+	Publisher_id = db.Column(db.Integer, db.ForeignKey('publisher.id'))
+	publisher = db.relationship('Publisher', backref=db.backref('books', lazy = True))
+
 	Pages = db.Column(Integer)
 	YearPublished = db.Column(Integer)
 
 	def __repr__(self):
 		return '<Book %r>' % self.ISBN
 
+#Author table
 class Author(db.Model):
-	Author_id = db.Column(db.Integer, primary_key=True)
+	id = db.Column(db.Integer, primary_key=True)
 	Name = db.Column(db.String(80), nullable=False)
 	BirthDate = db.Column(db.DateTime)
 	DeathDate = db.Column(db.DateTime)
@@ -26,8 +34,9 @@ class Author(db.Model):
 	def __repr__(self):
 		return '<Author %r>' % self.Name
 
+#Publisher table
 class Publisher(db.Model):
-	Publisher_id = db.Column(db.Integer, primary_key=True)
+	id = db.Column(db.Integer, primary_key=True)
 	Name = db.Column(db.String(80), nullable=False)
 	ParentCompany = db.Column(db.String(80))
 	ParentCountry = db.Column(db.String(80))
