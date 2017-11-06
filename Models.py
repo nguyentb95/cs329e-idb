a2b71549608964879e423f3083110f484c6a4532
sdtran <steventran@utexas.edu>
2017-11-06 12:27:13 -0600

    Added junction tables

--- Models.py
+++ Models.py
@@ -6,18 +6,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
 #Book table
-class Book(db.Model):
-	ISBN = db.Column(db.Integer, primary_key=True)
-	Title = db.Column(db.String(80), unique=True, nullable=False)
-
-	Author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
-	author = db.relationship('Author', backref=db.backref('books', lazy = True))
-
-	Publisher_id = db.Column(db.Integer, db.ForeignKey('publisher.id'))
-	publisher = db.relationship('Publisher', backref=db.backref('books', lazy = True))
-
+class Book(db.Model):\
+	id = db.Column(db.Integer, primary_key=True)
+	ISBN = db.Column(db.Integer)
+	Title = db.Column(db.String(250))
+	Google_id = db.Column(db.String(100))
 	Pages = db.Column(Integer)
 	YearPublished = db.Column(Integer)
+	Description = db.Column(db.String(1000))
+	Image = db.Column(db.String(1000))
 
 	def __repr__(self):
 		return '<Book %r>' % self.ISBN
@@ -25,11 +22,13 @@ class Book(db.Model):
 #Author table
 class Author(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	Name = db.Column(db.String(80), nullable=False)
+	Name = db.Column(db.String(80))
 	BirthDate = db.Column(db.DateTime)
-	DeathDate = db.Column(db.DateTime)
-	Country = db.Column(db.String(80))
-	Male = db.Column(db.Boolean)
+	Education = db.Column(db.String(80))
+	Nationality = db.Column(db.String(80))
+	Alma_mater = db.Column(db.String(80))
+	Wikipedia = db.Coumn(String(1000))
+	Image = db.Column(db.String(1000))
 
 	def __repr__(self):
 		return '<Author %r>' % self.Name
@@ -37,12 +36,35 @@ class Author(db.Model):
 #Publisher table
 class Publisher(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	Name = db.Column(db.String(80), nullable=False)
+	Name = db.Column(db.String(80))
 	ParentCompany = db.Column(db.String(80))
 	ParentCountry = db.Column(db.String(80))
-	2014Revenue = db.Column(db.Integer)
-	YearEstablished = db.Column(db.Integer)
+	Location = db.Column(db.String(80))
+	YearFounded = db.Column(db.Integer)
 	BookCount = db.Column(db.Integer)
+	Image = db.Column(db.String(1000))
+	Website = db.Column(db.String(250))
+	Description = db.Column(db.String(1000))
 
 	def __repr__(self):
 		return '<Publisher %r>' % self.Name
+
+#Junction Tables
+class BookAuthor(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+
+	Author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
+	author = db.relationship('Author', backref=db.backref('books', lazy = True))
+
+	Book_id = db.Column(db.Integer, db.ForeignKey('book.id'))
+	book = db.relationship('Book', backref=db.backref('books', lazy = True))
+
+class BookPublisher(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+
+	Publisher_id = db.Column(db.Integer, db.ForeignKey('publisher.id'))
+	publisher = db.relationship('Publisher', backref=db.backref('books', lazy = True))
+
+	Book_id = db.Column(db.Integer, db.ForeignKey('book.id'))
+	book = db.relationship('Book', backref=db.backref('books', lazy = True))
+
