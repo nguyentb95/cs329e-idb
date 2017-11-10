d93030a545ab4e6e8ec6a5278ef91404ec70a844
yyuto6012 <(yyabu0618@gmail.com)>
2017-11-09 20:55:54 -0600

    unit test for models

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import os
 import unittest
-from models import Base, Book, engine
+from models import Base, Book, Author, Publisher, engine
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -11,15 +11,39 @@ session = DBSession()
 
 class DBTestCases(unittest.TestCase):
     def test_source_insert_1(self):
-        s = Book(id='20', title = 'C++')
+        s = Book(isbn='20', title = 'C++', google_id = "45", datePublished = "2015-12-08", description = "It is a impressive book.", image="http://upload.wikimedia.org/wikipedia/en/thumb/6/6f/Pottermore.png/225px-Pottermore.png")
         session.add(s)
         session.commit()
 
 
-        r = session.query(Book).filter_by(id = '20').one()
-        self.assertEqual(str(r.id), '20')
+        r = session.query(Book).filter_by(isbn = '20').one()
+        self.assertEqual(str(r.isbn), '20')
 
-        session.query(Book).filter_by(id = '20').delete()
+        session.query(Book).filter_by(isbn = '20').delete()
+        session.commit()
+
+    def test_source_insert_2(self):
+        s = Author(name='harperlee', birthDate = '1965-07-31', education = "achelor of Arts", nationality = "British", alma_mater = "University of Exeter", wikipedia="https://en.wikipedia.org/wiki/J._K._Rowling", image="http://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/J._K._Rowling_2010.jpg/220px-J._K._Rowling_2010.jpg", description="oanne \"Jo\" Rowling, OBE, FRSL, pen names J. K. Rowling and Robert Galbraith, is a British novelist, screenwriter and film producer best known as the author of the Harry Potter fantasy series.")
+        session.add(s)
+        session.commit()
+
+
+        r = session.query(Author).filter_by(name = 'harperlee').one()
+        self.assertEqual(str(r.name), 'harperlee')
+
+        session.query(Author).filter_by(name= 'harperlee').delete()
+        session.commit()
+
+    def test_source_insert_3(self):
+        s = Publisher(name='Pottermore', parentCompany = 'C++', owner = "Yuto", location = "England", yearFounded = "1965-07-31", image="http://upload.wikimedia.org/wikipedia/en/thumb/6/6f/Pottermore.png/225px-Pottermore.png", website = "http://www.pottermore.com\nshop.pottermore.com", wikipedia= "http://www.pottermore.com\nshop.pottermore.com", description="Pottermore is the digital publishing, e-commerce, entertainment, and news company from J.K. Rowling and is the global digital publisher of Harry Potter and J.K. Rowling's Wizarding World.")
+        session.add(s)
+        session.commit()
+
+
+        r = session.query(Publisher).filter_by(name = 'Pottermore').one()
+        self.assertEqual(str(r.name), 'Pottermore')
+
+        session.query(Publisher).filter_by(name = 'Pottermore').delete()
         session.commit()
 
 if __name__ == '__main__':
