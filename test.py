8d9327eaa4e9216a7b26e3902a18fef6cebfb1aa
AZ-SU\az-su <tolanbien2@gmail.com>
2017-11-27 19:34:01 -0600

    tolans unit tests

--- test.py
+++ test.py
@@ -34,7 +34,18 @@ class DBTestCases(unittest.TestCase):
         session.query(Book).filter_by(isbn = '33').delete()
         session.commit()
 
+    def test_book_3(self):
+        s = Book(isbn='37', title = 'How to be bad', google_id = "22", datePublished = "2017-11-12", description = "Wrost book of all time.", image="https://cdn.business2community.com/wp-content/uploads/2016/03/Vd3MJo.jpg")
+        session.add(s)
+        session.commit()
 
+
+        r = session.query(Book).filter_by(isbn = '37').one()
+        self.assertEqual(str(r.isbn), '37')
+
+        session.query(Book).filter_by(isbn = '37').delete()
+        session.commit()
+        
     def test_author_1(self):
         s = Author(name='harperlee', birthDate = '1965-07-31', education = "Bachelor of Arts", nationality = "British", alma_mater = "University of Exeter", wikipedia="https://en.wikipedia.org/wiki/J._K._Rowling", image="http://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/J._K._Rowling_2010.jpg/220px-J._K._Rowling_2010.jpg", description="oanne \"Jo\" Rowling, OBE, FRSL, pen names J. K. Rowling and Robert Galbraith, is a British novelist, screenwriter and film producer best known as the author of the Harry Potter fantasy series.")
         session.add(s)
@@ -59,7 +70,17 @@ class DBTestCases(unittest.TestCase):
         session.query(Author).filter_by(name= 'cslewis').delete()
         session.commit()
 
+    def test_author_3(self):
+        s = Author(name='chuck', birthDate = '1898-12-29', education = "nothing", nationality = "welsh", alma_mater = "University of nowhere", wikipedia="https://en.wikipedia.org/wiki/C._S._Lewis", image="https://upload.wikimedia.org/wikipedia/en/1/1e/C.s.lewis3.JPG", description="Clive Staples Lewis was a British novelist, poet, academic, medievalist, literary critic, essayist, lay theologian, broadcaster, lecturer, and Christian apologist. He held academic positions at both Oxford University and Cambridge University.")
+        session.add(s)
+        session.commit()
 
+
+        r = session.query(Author).filter_by(name = 'chuck').one()
+        self.assertEqual(str(r.name), 'chuck')
+
+        session.query(Author).filter_by(name= 'chuck').delete()
+        session.commit()
 
     def test_publisher_1(self):
         s = Publisher(name='Pottermore', parentCompany = 'C++', owner = "Yuto", location = "England", yearFounded = "1965-07-31", image="http://upload.wikimedia.org/wikipedia/en/thumb/6/6f/Pottermore.png/225px-Pottermore.png", website = "http://www.pottermore.com\nshop.pottermore.com", wikipedia= "http://www.pottermore.com\nshop.pottermore.com", description="Pottermore is the digital publishing, e-commerce, entertainment, and news company from J.K. Rowling and is the global digital publisher of Harry Potter and J.K. Rowling's Wizarding World.")
@@ -85,6 +106,17 @@ class DBTestCases(unittest.TestCase):
         session.query(Publisher).filter_by(name = 'Scholastic').delete()
         session.commit()
 
+    def test_publisher_3(self):
+        s = Publisher(name='fakepub', parentCompany = 'fakepub', owner = "fakepub", location = "U.S.", yearFounded = "1900-10-22", image="https://www.dedicatedteacher.com/media/images/publishers/large/sch_700.jpg", website = "http://www.scholastic.com/home/", wikipedia= "https://en.wikipedia.org/wiki/Scholastic_Corporation", description="Scholastic Corporation is an American multinational publishing, education and media company known for publishing, selling, and distributing books and educational materials for schools, teachers, parents, and children. ")
+        session.add(s)
+        session.commit()
+
+
+        r = session.query(Publisher).filter_by(name = 'fakepub').one()
+        self.assertEqual(str(r.name), 'fakepub')
+
+        session.query(Publisher).filter_by(name = 'fakepub').delete()
+        session.commit()
 
 if __name__ == '__main__':
     unittest.main()
